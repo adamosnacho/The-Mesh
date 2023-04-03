929c80d50e5e7fb067e8d265692807289823bdc8
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-03 15:39:03 +0200

    Create upd.py

---
+++ upd.py
@@ -0,0 +1,19 @@
+import network,requests
+with open('wifi','r') as f:
+	WIFISET = eval(f.read())
+print('setup')
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect(WIFISET[0], WIFISET[1])
+
+def wof(name):
+    with open(name,'w') as f:
+        f.write(requests.get('https://raw.githubusercontent.com/adamosnacho/The-Mesh/apps/'+name))
+
+wof('main.py')
+wof('store.py')
+wof('WifiConfig.py')
+wof('console.py')
+wof('funcs')
+os.remove('upd.py')
+machine.reset()
