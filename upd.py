9d286f0250216759fe2d3d5eb767d2f1e5b84f85
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-03 21:33:08 +0200

    Update upd.py

--- upd.py
+++ upd.py
@@ -2,6 +2,10 @@ import requests
 
 
 def wof(name):
+    scr.clear()
+    scr.text('Installing:',0,0,1)
+    scr.text(name,0,10,1)
+    scr.show()
     with open(name,'w') as f:
         f.write(requests.get('https://raw.githubusercontent.com/adamosnacho/The-Mesh/Os/'+name).text)
 
