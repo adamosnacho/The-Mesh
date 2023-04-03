5802617f579862ba18a7a75cef309ae3e31d0d1f
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-03 22:05:35 +0200

    Update upd.py

--- upd.py
+++ upd.py
@@ -9,11 +9,11 @@ def wof(name):
     with open(name,'w') as f:
         f.write(requests.get('https://raw.githubusercontent.com/adamosnacho/The-Mesh/Os/'+name).text)
 
-if not sta_if.isconnected: machine.reset()
-wof('main.py')
-wof('store.py')
-wof('WifiConfig.py')
-wof('console.py')
-wof('funcs.py')
-os.remove('upd.py')
-machine.reset()
+if sta_if.isconnected:
+    wof('main.py')
+    wof('store.py')
+    wof('WifiConfig.py')
+    wof('console.py')
+    wof('funcs.py')
+    os.remove('upd.py')
+    machine.reset()
