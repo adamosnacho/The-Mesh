35d72ad20ecc8c0ab778c3625bd23f4e3190db4c
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-03 18:43:59 +0200

    Update upd.py

--- upd.py
+++ upd.py
@@ -1,17 +1,11 @@
-import network,requests
-with open('wifi','r') as f:
-	WIFISET = eval(f.read())
-print('setup')
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect(WIFISET[0], WIFISET[1])
+import requests
+
 
 def wof(name):
     with open(name,'w') as f:
         f.write(requests.get('https://raw.githubusercontent.com/adamosnacho/The-Mesh/Os/'+name).text)
 
-while not sta_if.isconnected:time.sleep(1)
-
+if not sta_if.isconnected: machine.reset()
 wof('main.py')
 wof('store.py')
 wof('WifiConfig.py')
