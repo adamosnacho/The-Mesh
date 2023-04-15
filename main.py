6ebcb578bb1780970e33a7ae94499682cbbdda07
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-15 05:09:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ scr = SSD1306_I2C(128, 64, i2c)
 
 #lists and sets all apps
 def upda():
-    apps = os.listdir()
-    if not Btn(1):
-        apps.remove('main.py')
-        apps.remove('funcs.py')
-        apps.remove('ssd1306.py')
-        apps.remove('boot.py')
-        apps.remove('console.py')
-        apps.remove('wifi')
-        apps.remove('requests.py')
-    return apps
+	apps = os.listdir()
+	if not Btn(1):
+		apps.remove('main.py')
+		apps.remove('funcs.py')
+		apps.remove('ssd1306.py')
+		apps.remove('boot.py')
+		apps.remove('console.py')
+		apps.remove('wifi')
+		apps.remove('requests.py')
+	return apps
 
 #wifi init
 Connected = False
 with open('wifi','r') as f:
-    WIFISET = eval(f.read())
+	WIFISET = eval(f.read())
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 try:
-    sta_if.connect(WIFISET[0], WIFISET[1])
+	sta_if.connect(WIFISET[0], WIFISET[1])
 except:pass
 
 #loading screen
@@ -44,29 +44,29 @@ On = True
 pn = 0
 
 while On:
-    Connected = sta_if.isconnected()
-    scr.clear()
-    for i in range(len(apps)):
-        if i == pn:
-            scr.text('> ' + apps[i].replace('.py', ''), 1, ((i * 10) - (pn * 10))+30, 1)
-        else:
-            scr.text(apps[i].replace('.py', ''), 1, ((i * 10) - (pn * 10))+30, 1)
-    scr.show()
-    if Btn(1):
-        pn -= 1
-        time.sleep(0.2)
-    if Btn(2):
-        pn += 1
-        time.sleep(0.2)
-    pn = Clamp(pn, 0, len(apps) - 1)
-    if Btn(4):
-        scr.clear()
-        scr.show()
-        time.sleep(0.01)
-        exec(RF(apps[pn]))
-        scr.clear()
-        scr.show()
-        time.sleep(0.01)
-        apps = upda()
+	Connected = sta_if.isconnected()
+	scr.clear()
+	for i in range(len(apps)):
+		if i == pn:
+			scr.text('> ' + apps[i].replace('.py', ''), 1, ((i * 10) - (pn * 10))+30, 1)
+		else:
+			scr.text(apps[i].replace('.py', ''), 1, ((i * 10) - (pn * 10))+30, 1)
+	scr.show()
+	if Btn(1):
+		pn -= 1
+		time.sleep(0.2)
+	if Btn(2):
+		pn += 1
+		time.sleep(0.2)
+	pn = Clamp(pn, 0, len(apps) - 1)
+	if Btn(4):
+		scr.clear()
+		scr.show()
+		time.sleep(0.01)
+		exec(RF(apps[pn]))
+		scr.clear()
+		scr.show()
+		time.sleep(0.01)
+		apps = upda()
 
-    time.sleep(0.005)
+	time.sleep(0.005)
