f288825f4f8beebb67d3349c41ab8194aca30bda
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-15 15:40:03 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,5 +1,5 @@
 from machine import Pin, I2C, ADC
-import ssd1306
+import ssd1306,framebuf
 
 def RF(name):return open(name, 'r').read()
 
@@ -21,3 +21,39 @@ def Clamp(x,Min,Max):
 
 def Map(x, IS, IE, OS, OE):
 	return OS + (OE - OS) * ((x - IS) / (IE - IS))
+def Bitmap(xs,ys,pix,scale):
+	lpix = []
+	pix = pix.replace(' ','')
+	
+	#lexer
+	for index in range(0, len(pix), xs):
+		lpix.append(pix[index : index + xs])
+	
+	#scale x
+	for i in range(len(lpix)):
+		newlp = ''
+		lp = lpix[i]
+		for char in range(len(lp)):
+			for res in range(scale):
+				newlp += lp[char]
+		lpix[i] = newlp
+	
+	#scale y
+	newlp = []
+	for i in range(len(lpix)):
+		for res in range(scale):
+			newlp.append(lpix[i])
+	lpix = newlp
+	pix = ''.join(lpix)
+	print(lpix)
+	fb = framebuf.FrameBuffer(bytearray((xs*scale) * (ys*scale)), (xs*scale), (ys*scale), framebuf.MONO_VLSB)
+	fb.fill(0)
+	cp = 0
+	for y in range(ys*scale):
+		for x in range(xs*scale):
+			fb.pixel(x,y,int(pix[cp]))
+			cp += 1
+	return(fb)
+
+
+
