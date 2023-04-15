038015edb8c82f9ea0899d23cba3f5c49b62e0b7
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-15 23:35:03 +0200

    Update remove.py

--- remove.py
+++ remove.py
@@ -39,4 +39,27 @@ def Keyboard(title):
 		scr.show()
 
 	return data
-os.remove(Keyboard('what?'))
+os.chdir('/')
+files = os.listdir()
+Picking = True
+pn = 0
+while Picking:
+	if Btn(1):
+		pn -= 1
+		time.sleep(0.1)
+	if Btn(2):
+		pn += 1
+		time.sleep(0.1)
+	pn = Clamp(pn,0,len(files)-2)
+	
+	scr.clear()
+	for i in range(len(files)-1):
+		if i == pn:scr.text('>'+files[i],0,((i * 10) - (pn * 10))+30,1)
+		else:scr.text(files[i],0,((i * 10) - (pn * 10))+30,1)
+	scr.show()
+	
+	if Btn(4):
+		file = files[pn]
+		while Btn(4):continue
+		Picking = False
+os.remove(file)
