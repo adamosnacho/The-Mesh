c06535c73ec66dedc6e0ee9a58a356f56c015bc9
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-04-15 12:41:12 +0200

    Update upd.py

--- upd.py
+++ upd.py
@@ -5,14 +5,14 @@ def wof(name):
 	scr.text(name,0,10,1)
 	scr.show()
 	cnt = requests.get('https://raw.githubusercontent.com/adamosnacho/The-Mesh/Os/'+name).text
-	if not sta_if.isconected():return
+	if not sta_if.isconnected():return
 	with open(name,'w') as f:
 		if cnt != '' or cnt != '404: Not Found':
 			f.write(cnt)
 if sta_if.isconnected():
 	scr.clear()
 	scr.text('Connection',0,0,1)
-	scr.text('estabilished.',0,10,1)
+	scr.text('established.',0,10,1)
 	scr.show()
 	time.sleep(2)
 	wof('main.py')
@@ -24,7 +24,8 @@ if sta_if.isconnected():
 	scr.text('Done!',0,0,1)
 	scr.show()
 	os.remove('upd.py')
-	time.sleep(4)
+	machine.reset()
+	time.sleep(15)
 else:
 	scr.clear()
 	scr.text('No Internet!',0,0,1)
