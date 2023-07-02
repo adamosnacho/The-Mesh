ddad55c1ec803e22d2d3f6779846fc2fee98f831
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2023-07-02 20:21:17 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -80,17 +80,19 @@ def InRange(x,minn,maxn):
 def PickList(choices,scr):
 	Picking = True
 	pn = 0
-	for i in range(len(choices)):
-		if i == pn:
-			scr.text('>' + choices[i], 1, ((i * 10) - (pn * 10))+30, 1)
-		else:
-			scr.text(choices[i], 1, ((i * 10) - (pn * 10))+30, 1)
-	if Btn(1):
-		pn -= 1
-		time.sleep(0.2)
-	if Btn(2):
-		pn += 1
-		time.sleep(0.2)
-	pn = Clamp(pn, 0, len(choices) - 1)
-	if Btn(4):
-		return choices[pn]
+	while Picking:
+		for i in range(len(choices)):
+			if i == pn:
+				scr.text('>' + choices[i], 1, ((i * 10) - (pn * 10))+30, 1)
+			else:
+				scr.text(choices[i], 1, ((i * 10) - (pn * 10))+30, 1)
+		if Btn(1):
+			pn -= 1
+			time.sleep(0.2)
+		if Btn(2):
+			pn += 1
+			time.sleep(0.2)
+		pn = Clamp(pn, 0, len(choices) - 1)
+		if Btn(4):
+			Picking = False
+			return choices[pn]
